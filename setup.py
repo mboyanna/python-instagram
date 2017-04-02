ce72bfdb7ccaf45054b340be1af561ecdb578947
Bojana Parman <mboyanna@gmail.com>
2017-04-02 16:12:03 -0700

    Changed version to 1.3.2.1

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@
 from setuptools import setup, find_packages
 
 setup(name="python-instagram",
-      version="1.3.2",
+      version="1.3.2.1",
       description="Instagram API client",
       license="MIT",
       install_requires=["simplejson","httplib2","six"],
       author="Instagram, Inc",
       author_email="apidevelopers@instagram.com",
-      url="http://github.com/Instagram/python-instagram",
+      url="https://github.com/mboyanna/python-instagram.git",
       packages = find_packages(),
       keywords= "instagram",
       zip_safe = True)
